365751c07ef5a86ce9bba62d4d7c0f25eec886c8
Nicholas Cramer <73135495+NicholasCramer@users.noreply.github.com>
2023-11-07 16:22:21 -0700

    Day17_QuizGame

---
+++ Day17_QuizGame/data.py
@@ -0,0 +1,40 @@
+# question data generated using https://opentdb.com/ api
+
+question_data = [
+
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the 1988 film 'Akira', Tetsuo ends up destroying Tokyo.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "Clefairy was intended to be Ash's starting Pokemon in the "
+                 "pilot episode of the cartoon.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "The anime 'Lucky Star' follows the story of one girl who is unaware she is God.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "No Game No Life first aired in 2014.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the 'Melancholy of Haruhi Suzumiya' series, the narrator goes by the nickname Kyon.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In Pokemon, Ash's Pikachu refuses to go into a pokeball.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the 'Toaru Kagaku no Railgun' anime,  espers can only reach "
+                 "a maximum of level 6 in their abilities.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "The name of the attack 'Kamehameha' in Dragon Ball Z"
+                 " was named after a famous king of Hawaii.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "Kiznaiver is an adaptation of a manga.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the 'To Love-Ru' series, Golden Darkness is sent to kill Lala Deviluke.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}
+]
