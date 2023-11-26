23d15a0706fe9c68d11bf8369345a1b638df0a79
Nicholas Cramer <73135495+NicholasCramer@users.noreply.github.com>
2023-11-26 10:07:18 -0700

    Day26_nato-alphabet
    List and dictionary comprehension

---
+++ Day26_nato-alphabet/main.py
@@ -0,0 +1,11 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Please enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in user_input]
+print(output_list)
