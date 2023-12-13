1c65dd1f769083fc83d230388c2f7ddf27465ddb
Nicholas Cramer <73135495+NicholasCramer@users.noreply.github.com>
2023-12-13 15:36:27 -0700

    remove repeated import statement

--- Day39/flight-deal-finder/main.py
+++ Day39/flight-deal-finder/main.py
@@ -12,9 +12,6 @@ print(sheet_data)
 ORIGIN_CITY_IATA = "DEN"
 
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
     print(f"sheet_data:\n {sheet_data}")
